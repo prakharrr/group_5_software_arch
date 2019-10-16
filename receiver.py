15cbb3f6016757d1c6180ee7b4148985ae909f72
prakharrr <prakharsens@gmail.com>
2019-10-16 12:24:52 -0400

    pushing changes before I break things

--- receiver.py
+++ receiver.py
@@ -2,9 +2,13 @@
 
 import pika
 import time
+from datetime import datetime
 import multiprocessing
 import logging
 import sys
+from pika.compat import unicode_type, dictkeys, is_integer
+TIME_FORMAT = ("%m/%d/%Y %H:%M:%S")
+now = (datetime.now().strftime(TIME_FORMAT))
 #
 #Optimize and use a failure on auto_ack or callback 
 # Also, cleanup is required + argsparse + commandline args
@@ -22,21 +26,20 @@ channel.queue_declare(Q)
 
 def callback(ch, method, properties, body):
 
-	for line in body:
-		if not line:
-			ch.basic_ack(delivery_tag = method.delivery_tag)
-			print('NACK')
-		else:
-			ch.basic_nack(delivery_tag = method.delivery_tag) 
-			print('ACK')
-	print("delivery tag: {0}\tchannel num: {1}\tReceived body: {2}".format(method.delivery_tag, ch.channel_number,\
-	body.decode()))
-	ch.basic_ack(delivery_tag=method.delivery_tag)
-
+	# time.sleep(5)
+	body = body.decode('utf-8')
+	for line in (body.split(',')):
+		if 'Result' in line:
+		    print ('Received data: {}'.format(line))
+		elif 'ERROR' in line:
+			print('[*] Error detected in the sender at -- {}'.format(now))
+			print('[MONITOR] An error has been detected at the sender\'s end. \nPlease check the system and run again')
+			# sys.exit(1)
+	print("Received: delivery tag: {0}\tchannel num: {1}".format(method.delivery_tag, ch.channel_number))
 
 print("Ready to receive message from {} queue.".format(Q))
 print(' [*] Waiting for messages. To exit press CTRL+C')
 
-channel.basic_consume(on_message_callback=callback,queue=Q, auto_ack=False)
+channel.basic_consume(on_message_callback=callback,queue=Q, auto_ack=True)
 
 channel.start_consuming()
--- sender.py
+++ sender.py
@@ -20,18 +20,28 @@ conn = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
 channel = conn.channel()
 
 channel.queue_declare(Q)
-channel.confirm_delivery()
+
 
 def failure():
-	log = ''
-	for i in range(0,10)[::-1]:
-		time.sleep(1)
-		print('Result is {}/{}= {}'.format(i,i-1,i//(i-1)))
-		log.join('Result is {}/{}= {}'.format(i,i-1,i//(i-1)))
-		print("[MONITOR] Sent Data data at -- {}".format(now))
-	return(log)
+	isAlive=True
+	log = []
+	err_log = []
+	try:
+		for i in range(1,10)[::-1]:
+			time.sleep(1)
+			print('Result is {}/{}= {}'.format(i,i-1,i//(i-1)))
+			log.append('Result is {}/{}= {}'.format(i,i-1,i//(i-1)))
+			print("[MONITOR] Sent Data data at -- {}".format(now))
+	except:
+		isAlive=False
+		print('[MONITOR] Problem detected in the sender -- {}'.format(now))
+		print('[MONITOR] Could not send the result to receiver -- {}'.format(now))
+		err_log.append('[ERROR] Error detected in the sender.')
+	return str(log) if isAlive else str(err_log)
 
 
 channel.basic_publish(exchange="", routing_key=Q, body=failure(), properties=pika.BasicProperties(delivery_mode=1))
+# channel.basic_publish(exchange="", routing_key=Q, body=failure(), properties=pika.BasicProperties(delivery_mode=1))
+# channel.basic_publish(exchange="", routing_key=Q, body=failure(), properties=pika.BasicProperties(delivery_mode=1))
 
 conn.close()
