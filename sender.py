f82c38db1bb6995677ffc3bb7f2c54d4cb5cc198
Nevets44 <sds9278@g.rit.edu>
2019-10-26 13:56:24 -0400

    Adding monitor and modified sender to replicate partial passive spare behavior

--- sender.py
+++ sender.py
@@ -1,31 +1,76 @@
+# -*- coding: utf-8 -*-
+
 import pika
 from datetime import datetime
-from random import randint
-from time import sleep
+import random
 
+import time
 TIME_FORMAT = ("%m/%d/%Y %H:%M:%S")
 now = (datetime.now().strftime(TIME_FORMAT))
-numerator = randint(0, 100)
-denominator = randint(0,10)
+
 
 Q="PIKA_QUEUE"
+
 # DEFINE ROUTING KEY FOR SECURITY LATER
+
 # ROUTING_KEY = ""
 
-conn = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
 
-channel = conn.channel()
+def not_failure():
+	"""
+	Data which doesn't result in errors at the receiver
+	"""
+	isAlive=True
+	log = []
+	err_log = []
+	try:
+		for i in range(2,10)[::-1]:
+			time.sleep(1)
+			print('Result is {}/{}= {}'.format(i,i-1,i//(i-1)))
+			log.append('Result is {}/{}= {}'.format(i,i-1,i//(i-1)))
+			print("[MONITOR] Sent Data data at -- {}".format(now))
+	except:
+		isAlive=False
+		print('[MONITOR] Problem detected in the sender -- {}'.format(now))
+		print('[MONITOR] Could not send the result to receiver -- {}'.format(now))
+		err_log.append('[ERROR] Error detected in the sender.')
+	return str(log) if isAlive else str(err_log)
 
-channel.queue_declare(Q)
+def failure():
+	"""
+	Data which does result in errors at the receiver
+	"""
+	isAlive=True
+	log = []
+	err_log = []
+	try:
+		for i in range(1,10)[::-1]:
+			time.sleep(1)
+			print('Result is {}/{}= {}'.format(i,i-1,i//(i-1)))
+			log.append('Result is {}/{}= {}'.format(i,i-1,i//(i-1)))
+			print("[MONITOR] Sent Data data at -- {}".format(now))
+	except:
+		isAlive=False
+		print('[MONITOR] Problem detected in the sender -- {}'.format(now))
+		print('[MONITOR] Could not send the result to receiver -- {}'.format(now))
+		err_log.append('[ERROR] Error detected in the sender.')
+	return str(log) if isAlive else str(err_log)
 
-while True:
-    if denominator == 0:
-        print("I fail now")
-    numerator / denominator
-    channel.basic_publish(exchange="", routing_key=Q,  body="Every Pizza is a personal pizza if you believe in yourself!!!")
-    print("[MONITOR] Sent Beep boop beeep at -- {}".format(now))
-    numerator = randint(0, 100)
-    denominator = randint(0, 2)
-    sleep(1)
+def sendMessage():
+    conn = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
 
-conn.close()
+    channel = conn.channel()
+
+    channel.queue_declare(Q)
+
+    channel.basic_publish(exchange="", routing_key=Q, body=failure(), properties=pika.BasicProperties(delivery_mode=1))
+
+    # If failure occurs, run this
+    # channel.basic_publish(exchange="", routing_key=Q, body=not_failure(), properties=pika.BasicProperties(delivery_mode=1))
+    # channel.basic_publish(exchange="", routing_key=Q, body=failure(), properties=pika.BasicProperties(delivery_mode=1))
+
+    print("FAIL")
+    conn.close()
+
+if __name__ == '__main__':
+    sendMessage()
