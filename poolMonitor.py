f05c9dc269c468d79c3c43a515e94f2adc4fcb4c
Nevets44 <sds9278@g.rit.edu>
2019-11-11 16:24:37 -0500

    Added the PriorityThread class and created the queue for adding it

--- poolMonitor.py
+++ poolMonitor.py
@@ -1,35 +1,29 @@
-from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
-from filecmp import cmp
-from queue import Queue, PriorityQueue
+from concurrent.futures import ThreadPoolExecutor
+from queue import PriorityQueue
 from random import randint
-import threading
+from threading import Thread
 import time
 
-fibonaci = 0
+class PriorityThread(Thread):
 
-class PriorityTask(object):
     def __init__(self, priority, name):
+        Thread.__init__(self)
         self.priority = priority
         self.name = name
-
-    def __cmp__(self, other):
-        return cmp(self.priority, other.priority)
 
     def __lt__(self, other):
         return self.priority < other.priority
 
+    def __str__(self):
+        return self.name + ": " +  str(self.priority)
+
+    def start(self):
+        fib()
+
 def fib():
     print("Fibonacci")
 
-def main():
-    with ProcessPoolExecutor(max_workers=3) as pool:
-        pool.submit(fib)
-
-
 if __name__ == '__main__':
-    jobs = []
-    print("Hello")
-
     q = PriorityQueue()
 
     with ThreadPoolExecutor(max_workers = 10) as pool:
@@ -38,20 +32,18 @@ if __name__ == '__main__':
         threadArr = []
 
         for i in range(10):
-            threadArr.append(threading.Thread(name='Thread-', target=fib, args=''))
-            q.put(PriorityTask(i, 'Yes'))
-            print(randint(1, 10))
+            threadArr.append(Thread(name='Thread-', target=fib, args=''))
+            q.put(PriorityThread(i+1, 'Thread-' + str(i+1)))
 
-        print(threadArr.__len__())
+        #print(threadArr.__len__())
         #q.put({7, threading.Thread(name='monitor', target=fib, args='')})
         #q.put({5, threading.Thread(name='monitor', target=fib, args='')})
         #q.put({10, threading.Thread(name='monitor', target=fib, args='')})
 
 
-        print('Not dead yet')
         while q.qsize() > 0:
             print('queue size:', q.qsize())
-            print(q.queue)
-            print(q.get())
+            print(*q.queue)
+            print('Popped: ', q.get())
             time.sleep(1)
 
